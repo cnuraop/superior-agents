3153ff343ce1e9f4bda6951200514aa536184004
cnuraop <srikanth8605@gmail.com>
2025-07-01 09:09:41 +0530

    Update ChatBox.tsx

--- agent/hackmate-copilot/frontend/src/components/ChatBox.tsx
+++ agent/hackmate-copilot/frontend/src/components/ChatBox.tsx
@@ -37,7 +37,7 @@ export default function ChatBox() {
 
     setIsLoading(true)
     try {
-      const res = await axios.post("http://agentcopilot.netlify.app:9009/query", {
+      const res = await axios.post("https://agentcopilot.netlify.app:9009/query", {
         message: query,
       })
       setResponse(res.data.reply)
